9fdb2460566a883349203a2205ef998ce84ce034
Park Hyunwoo <ez@smartstudy.co.kr>
2013-05-14 05:22:33 +0900

    First release

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ pelican-embed-tweet
 ===================
 
 Embedding tweets into your Pelican blog posts.
+
+
+How to use this?
+----------------
+
+ 1. Download `embed_tweet.py`
+ 1. Copy it wherever or just into `pelican/plugins/`
+ 1. Push back `embed_tweet` into plugin list of settings.
+    - `PLUGINS = ['pelican.plugins.embed_tweet']`
+
+ 1. That's all!
+
+
+Conversion
+----------
+
+ 1. `@username` will be replaced by `https://twitter.com/username`
+ 1. `@username/status/tweetid` will be replaced by `Embedded-tweet`
+    - See: <https://dev.twitter.com/docs/embedded-tweets>
+
+
+License
+-------
+
+Distributed under MIT license.
+
+
+AUTHOR
+------
+Park Hyunwoo / [@lqez](https://twitter.com/lqez)
---
+++ embed_tweet.py
@@ -0,0 +1,39 @@
+"""
+Embedded tweet plugin for Pelican
+=================================
+
+This plugin allows you to embed Twitter tweets into your articles.
+And also provides a link for Twitter username.
+
+    i.e.
+
+        @username
+
+        will be replaced by a link to Twitter username page.
+
+        @username/status/tweetid
+
+        will be replaced by a `Embedded-tweet`_ API.
+
+.. _Embedded-tweet: https://dev.twitter.com/docs/embedded-tweets
+
+"""
+
+from pelican import signals
+import re
+
+
+def embed_tweet(sender, instance):
+    instance._content = re.sub(
+        r'(^|[^@\w])@(\w{1,15})\b',
+        '\\1<a href="https://twitter.com/\\2">@\\2</a>',
+        re.sub(
+            r'(^|[^@\w])@(\w{1,15})/status/(\d+)\b',
+            '\\1<blockquote class="twitter-tweet" align="center"><a href="https://twitter.com/\\2/status/\\3">Tweet of \\2/\\3</a></blockquote>',
+            instance._content
+        )
+    ) + '<script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
+
+
+def register():
+    signals.content_object_init.connect(embed_tweet)
