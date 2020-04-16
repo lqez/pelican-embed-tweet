e9e50f58ef0ff0c8f90d3b8c8c683966d3fab674
Hyunwoo Park <ez.amiryo@gmail.com>
2020-04-16 17:46:09 +0900

    Add prefix `t`

--- embed_tweet.py
+++ embed_tweet.py
@@ -7,11 +7,11 @@ And also provides a link for Twitter username.
 
     i.e.
 
-        @username
+        t@username
 
         will be replaced by a link to Twitter username page.
 
-        @username/status/tweetid
+        t@username/status/tweetid
 
         will be replaced by a `Embedded-tweet`_ API.
 
@@ -28,11 +28,11 @@ def embed_tweet(instance):
         return
 
     instance._content = re.sub(
-        r'(^|[^@\w])@(\w{1,15})(\b[^/])',
-        '\\1<a href="https://twitter.com/\\2">@\\2</a>\\3',
+        r'(^|[^/])(t)@(\w{1,15})(\b[^\/])',
+        '\\1<a href="https://twitter.com/\\3">@\\3</a>\\4',
         re.sub(
-            r'(^|[^@\w])@(\w{1,15})/status/(\d+)\b',
-            '\\1<blockquote class="twitter-tweet" align="center"><a href="https://twitter.com/\\2/status/\\3">Tweet of \\2/\\3</a></blockquote>',
+            r'(^|[^/])(t)@(\w{1,15})/status/(\d+)\b',
+            '\\1<blockquote class="twitter-tweet" align="center"><a href="https://twitter.com/\\3/status/\\4">Tweet of \\3/\\4</a></blockquote>',
             instance._content
         )
     ) + '<script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
