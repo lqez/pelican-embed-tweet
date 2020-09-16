b6d8f550885e7df71e7ac389f7ee2dc7a7e726d2
I-Kang Ding <ikding@gmail.com>
2020-09-15 17:53:04 -0700

    Change embed-tweet prefix to t# to work with RST files
    Related issue: https://github.com/lqez/pelican-embed-tweet/issues/5

--- embed_tweet.py
+++ embed_tweet.py
@@ -7,11 +7,11 @@ And also provides a link for Twitter username.
 
     i.e.
 
-        t@username
+        t#username
 
         will be replaced by a link to Twitter username page.
 
-        t@username/status/tweetid
+        t#username/status/tweetid
 
         will be replaced by a `Embedded-tweet`_ API.
 
@@ -31,7 +31,7 @@ def embed_tweet(instance):
         r'(^|[^/])(t)@(\w{1,15})(\b[^\/])',
         '\\1<a href="https://twitter.com/\\3">@\\3</a>\\4',
         re.sub(
-            r'(^|[^/])(t)@(\w{1,15})/status/(\d+)\b',
+            r'(^|[^/])(t)#(\w{1,15})/status/(\d+)\b',
             '\\1<blockquote class="twitter-tweet" align="center"><a href="https://twitter.com/\\3/status/\\4">Tweet of \\3/\\4</a></blockquote>',
             instance._content
         )
